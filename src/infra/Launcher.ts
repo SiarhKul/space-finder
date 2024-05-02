a5c6dd0db8d482057a61d7890f72c8f33a5c0317
siarhkul <siarhkul@gmail.com>
2024-05-02 19:11:04 +0200

    feat: Log

--- src/infra/Launcher.ts
+++ src/infra/Launcher.ts
@@ -6,6 +6,7 @@ import {ApiStack} from "./stacks/ApiStack";
 const app: App = new App()
 
 const DBTableStack:DataStack = new DataStack(app, 'DataStack')
+console.log("=>(Launcher.ts:9) DBTableStack", DBTableStack);
 const lambdaStack: LambdaStack = new LambdaStack(app, 'LambdaStack',{
     spaceTable: DBTableStack.spacesTable
 });

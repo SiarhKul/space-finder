a91448598b7388326f7981ca9afe4528800f2d1c
siarhkul <siarhkul@gmail.com>
2024-05-06 14:17:28 +0200

    feat: Fix amplify

--- test/AuthService.ts
+++ test/AuthService.ts
@@ -1,20 +1,22 @@
-import {Amplify, Auth} from "aws-amplify";
-import {type CognitoUser} from "@aws-amplify/auth";
+import { type CognitoUser } from '@aws-amplify/auth';
+import { Amplify, Auth } from 'aws-amplify';
 
 Amplify.configure({
     Auth: {
         region: 'eu-central-1',
-        userPoolArn: 'eu-central-1_KsPTLTTb7',
+        userPoolId: 'eu-central-1_KsPTLTTb7',
         userPoolWebClientId: '7mc3b84baub4o43jrgsja8ad4q',
         authenticationFlowType: 'USER_PASSWORD_AUTH'
-
     }
-})
+});
+
+
 
 export class AuthService {
-    public async login(useName: string, password: string) {
 
-        const result: CognitoUser = await Auth.signIn(useName, password) as CognitoUser
-        return result
+
+    public async login(userName: string, password: string) {
+        const result = await Auth.signIn(userName, password) as CognitoUser;
+        return result;
     }
 }

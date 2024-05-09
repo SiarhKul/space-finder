8fd398ed8c701a15c226fb28d504329355239f30
siarhkul <siarhkul@gmail.com>
2024-05-09 18:34:04 +0200

    feat: Identiry pool

--- test/AuthService.ts
+++ test/AuthService.ts
@@ -1,20 +1,41 @@
-import {type CognitoUser} from '@aws-amplify/auth';
+import type {CognitoUser} from '@aws-amplify/auth';
 import {Amplify, Auth} from 'aws-amplify';
+import {CognitoIdentityClient} from "@aws-sdk/client-cognito-identity";
+import {fromCognitoIdentityPool} from "@aws-sdk/credential-providers"
 
+const awsRegion = 'eu-central-1'
 Amplify.configure({
   Auth: {
-    region: 'eu-central-1',
+    region: awsRegion,
     userPoolId: 'eu-central-1_KsPTLTTb7',
     userPoolWebClientId: '7mc3b84baub4o43jrgsja8ad4q',
-    authenticationFlowType: 'USER_PASSWORD_AUTH'
+    authenticationFlowType: 'USER_PASSWORD_AUTH',
+    identityPoolId: 'eu-central-1:f3306170-bdf1-4cdc-9858-bce226559aeb'
   }
 });
 
 
 export class AuthService {
-
   public async login(userName: string, password: string) {
     const result = await Auth.signIn(userName, password) as CognitoUser;
     return result;
   }
+
+  public async generateTemporaryCredentials(user: CognitoUser) {
+    const jwtToken: string | undefined = user.getSignInUserSession()?.getIdToken().getJwtToken()
+    const cognitoIdentityPool: string = `cognito-idp.${awsRegion}.amazonaws.com/eu-central-1_KsPTLTTb7`
+    const cognitoIdentity: CognitoIdentityClient = new CognitoIdentityClient({
+      credentials: fromCognitoIdentityPool({
+        identityPoolId: 'eu-central-1:f3306170-bdf1-4cdc-9858-bce226559aeb',
+        logins: {
+          [cognitoIdentityPool]: jwtToken ?? ''
+        }
+      })
+
+    })
+    const credantions = await cognitoIdentity.config.credentials()
+    return credantions
+  }
+
+
 }
--- test/auth.test.ts
+++ test/auth.test.ts
@@ -2,13 +2,16 @@ import {AuthService} from "./AuthService";
 import {CognitoUser} from "@aws-amplify/auth";
 
 
-async function testAuth():Promise<void> {
-    const service:AuthService = new AuthService()
-    const loginResult:CognitoUser = await service.login('sk', '1qaz@WSX')
+async function testAuth(): Promise<void> {
+  const service: AuthService = new AuthService()
+  const loginResult: CognitoUser = await service.login('sk', '1qaz@WSX')
 
-    // console.log("=>(auth.test.ts:6) service", loginResult);
-    console.log( loginResult.getSignInUserSession()?.getIdToken().getJwtToken());
+  // console.log("=>(auth.test.ts:6) service", loginResult);
+  const jwtToken = loginResult.getSignInUserSession()?.getIdToken().getJwtToken()
+  console.log('JWT_TOKEN',jwtToken);
 
+  const credantions = await service.generateTemporaryCredentials(loginResult)
+  console.log('CREDANTIONS', credantions)
 }
 
 testAuth().then(r => console.log(r))

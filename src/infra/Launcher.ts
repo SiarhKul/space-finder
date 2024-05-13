32cd00116ac69be21f3816eed48addc24f761f11
siarhkul <siarhkul@gmail.com>
2024-05-13 17:54:36 +0200

    feat: Add UI deployment

--- src/infra/Launcher.ts
+++ src/infra/Launcher.ts
@@ -3,6 +3,7 @@ import {DataStack} from "./stacks/DataStack";
 import {LambdaStack} from "./stacks/LambdaStack";
 import {ApiStack} from "./stacks/ApiStack";
 import {AuthCognitoStack} from "./stacks/AuthCognitoStack";
+import {UiDeploymentStack} from "./stacks/UiDeploymentStack";
 
 const app: App = new App()
 
@@ -18,3 +19,4 @@ new ApiStack(app, "ApiStack", {
     userPool: authCognitoStack.userPool,
     spacesLambdaIntegration: lambdaStack.spacesLambdaIntegration})
 
+new UiDeploymentStack(app, 'UiDeploymentStack')

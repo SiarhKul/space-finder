83578baf17c3cc9995c81804eb73dddadc1a9b85
siarhkul <siarhkul@gmail.com>
2024-05-18 12:45:07 +0200

    feat: Use spyes

---
+++ test/services/handler.test.ts
@@ -0,0 +1,47 @@
+import {handler} from "../../src/services/monitor/handler";
+
+
+describe('GIVEN Monitor lambda', () => {
+  const fetchSpy = jest.spyOn(global, 'fetch')
+  fetchSpy.mockImplementation(() => Promise.resolve({} as any))
+
+  afterEach(()=>{
+    jest.clearAllMocks();
+  })
+
+  test('SHOULD makes request for records in SNSEvent', async () => {
+    const snsEvent = {
+      Records: [{
+        Sns: {
+          Message: 'Test message'
+        }
+      }
+      ]
+    } as any
+
+
+    await handler(snsEvent, {})
+
+    expect(fetchSpy).toHaveBeenCalled()
+    expect(fetchSpy).toHaveBeenCalledWith(expect.any(String), {
+      method: "POST",
+      body: JSON.stringify({
+        "text": `SNS ${'Test message'}`
+      })
+    })
+
+  })
+
+  test('SHOULD no be called SNSEvent', async () => {
+    const snsEvent2 = {
+      Records: []
+    } as any
+
+    await handler(snsEvent2, {})
+
+    expect(fetchSpy).not.toHaveBeenCalled()
+
+  })
+
+
+});
